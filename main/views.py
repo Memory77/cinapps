30044907eb133f89b075f1aae933973859988ea2
Deborah <soccio.deborah@gmail.com>
2024-04-16 11:00:42 +0200

    màjs

--- main/views.py
+++ main/views.py
@@ -6,6 +6,8 @@ from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import os
 import json
+import mysql.connector
+from .database import connect_to_azure_mysql
 
 def home_page(request):
     return render(request, 'main/home_page.html')
@@ -16,4 +18,24 @@ def chiffre_page(request):
 
 @login_required
 def archive_page(request):
+    # Connexion à la base de données MySQL Azure
+    conn = connect_to_azure_mysql()
+    
+    if conn:
+        try:
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute("SELECT titre, description, image, date_sortie, genre, entrees, entrees_pred FROM films order by date_sortie DESC")
+            films = cursor.fetchall()
+            cursor.close()
+            conn.close()
+            
+            # Traitez les données récupérées comme vous le souhaitez
+            # Par exemple, renvoyez les films à votre modèle
+            return render(request, "main/archive_page.html", {"films": films})
+        except mysql.connector.Error as e:
+            print(f"Erreur lors de l'exécution de la requête SQL: {e}")
+    else:
+        print("La connexion à la base de données n'a pas été établie avec succès.")
+        # Gérez cette erreur en conséquence dans votre vue
+
     return render(request, "main/archive_page.html")
